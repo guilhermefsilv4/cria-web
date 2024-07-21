677383b64893b97edc5b3b44f793045dd695b360
GuilhermeSilva7930 <guilhermeferreira7930@gmail.com>
2024-07-21 01:02:43 -0300

    fotos mais leves, sobre e scroll reveal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonComponent } from '@components/button/button.component';
-import { MenuComponent } from "@components/menu/menu.component";
-import { MainComponent } from "./screens/main/main.component";
+import { MenuComponent } from '@components/menu/menu.component';
+import { MainComponent } from './screens/main/main.component';
+import ScrollReveal from 'scrollreveal';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, ButtonComponent, MenuComponent, MainComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet, ButtonComponent, MenuComponent, MainComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'cria-web';
-
-  teste() {
-    console.log("chamei")
-    alert("botão 1")
-  }
+    slideUp = {
+        distance: '150%',
+        origin: 'left',
+        opacity: 1,
+        easing: 'ease-in-out',
+        reset: true,
+    };
+    ngOnInit() {
+        ScrollReveal().reveal('.main-text', {
+            ...this.slideUp,
+            origin: 'left',
+        });
+        ScrollReveal().reveal('.photo-container', {
+            ...this.slideUp,
+            origin: 'right',
+        });
+        ScrollReveal().reveal('.title', { ...this.slideUp, origin: 'bottom' });
+    }
 }
--- src/app/screens/main/components/maincontent/maincontent.component.ts
+++ src/app/screens/main/components/maincontent/maincontent.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-import { ButtonComponent } from "../../../../components/button/button.component";
-import { MainButtonComponent } from "../mainbutton/button.component";
+import { ButtonComponent } from '@components/button/button.component';
+import { MainButtonComponent } from '../mainbutton/button.component';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
-  selector: 'app-maincontent',
-  standalone: true,
-  imports: [ButtonComponent, MainButtonComponent],
-  templateUrl: './maincontent.component.html',
-  styleUrl: './maincontent.component.scss'
+    selector: 'app-maincontent',
+    standalone: true,
+    imports: [ButtonComponent, MainButtonComponent, NgOptimizedImage],
+    templateUrl: './maincontent.component.html',
+    styleUrl: './maincontent.component.scss',
 })
-export class MainContentComponent {
-
-}
+export class MainContentComponent {}
