e4261b90f99f85d3018ba76921623dcbdade8b42
GuilhermeSilva7930 <guilhermeferreira7930@gmail.com>
2024-07-30 22:38:41 -0300

    main inclusao de telas contato, sobre, pilares e motivos

---
+++ src/app/screens/pillars/pillars.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core'; 
+import { NgOptimizedImage } from '@angular/common';
+
+@Component({
+  selector: 'app-pillars',
+  standalone: true,
+  imports: [NgOptimizedImage],
+  templateUrl: './pillars.component.html',
+  styleUrl: './pillars.component.scss'
+})
+export class PillarsComponent {
+
+}
