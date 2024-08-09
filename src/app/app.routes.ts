d4e9b69e92f5ae5916bc18fe6436b62962a8b67c
GuilhermeSilva <guilherme.silva@klab.com.br>
2024-08-09 20:10:14 -0300

    feature(framework): lazy load [main]

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: "home",
+    loadComponent: () => import("@screens/home/home.component").then((m) => m.HomeComponent)
+  },
+  {
+    path: "portfolio",
+    loadComponent: () => import("@screens/portfolio/portfolio.component").then((m) => m.PortfolioComponent)
+  },
+  {
+    path: "sobre",
+    loadComponent: () => import("@screens/aboutus/aboutus.component").then((m) => m.AboutusComponent)
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  },
+];
